6b60a03632d9fed1fb962c2f2413bc6a2f0ed813
acilll12 <87960815+acilll12@users.noreply.github.com>
2021-07-28 19:52:13 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "@only_fxc7"
+var creator = "AcillGans"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["Fxc7", "manogay"];
+const listkey = ["AcillGans", "luGay"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
@@ -504,7 +504,7 @@ router.get('/stalk/npm', async (req, res, next) => {
                  result
              })
          })
-         .catch(e => {
+         .catch(e => {#
          	res.json(loghandler.error)
 })
 } else {
